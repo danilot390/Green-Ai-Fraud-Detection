3c6935f85e728d53b1b36a767e306c2247b51686
danilot390 <danilot390@gmail.com>
2025-08-11 20:16:17 +0100

    Add preprocessing pipeline and dataset saving modified: -  added global settings for data preprocessing. -  added  library for handling imbalanced data. new: - Created folders  &   to store processed data following best practices. - Added  in src to treat it as a package. - Added  to execute and save the preprocessing steps. - Added  implementing:    * :  Load raw data from the specified file path.    * :  Handle missing values in the DataFrame.    * :  Apply feature engineering functions to the DataFrame.    * :  Scale numerical features in the training, validation, and test sets.    * :  Applies resampling techniques to handle class imbalance in training data.    * :  Splits the DataFrame into training, validation, and test sets and also uses stratification for general data and a temporal split for the synthetic dataset.    * :  Run the entire data preprocessing pipeline.    * : Custom Dataset class for fraud detection data. - Updated  with  function to load YAML configuration file.

---
+++ src/data/make_dataset.py
@@ -0,0 +1,63 @@
+import os 
+import torch
+import joblib 
+
+from src.data.preprocess import get_preprocessed_data, PROCESSED_DATA_PATH
+from src.utils.config_parser import load_config
+
+if __name__ == '__main__':
+    print("Running make dataset...")
+    
+    print('\n --- Processing Credit Card Fraud Dataset ---')
+    try:
+        ccf_processed_dir = PROCESSED_DATA_PATH['credit_card_fraud_path']
+
+        X_train, y_train, X_val, y_val, X_test, y_test, scaler, feature_names = \
+            get_preprocessed_data(dataset_name="credit_card_fraud", target_column="Class")
+        
+        # Save Tensors 
+        torch.save(X_train, os.path.join(ccf_processed_dir, 'X_train.pt'))
+        torch.save(y_train, os.path.join(ccf_processed_dir, 'y_train.pt'))
+        torch.save(X_val, os.path.join(ccf_processed_dir, 'X_val.pt'))
+        torch.save(y_val, os.path.join(ccf_processed_dir, 'y_val.pt'))
+        torch.save(X_test, os.path.join(ccf_processed_dir, 'X_test.pt'))
+        torch.save(y_test, os.path.join(ccf_processed_dir, 'y_test.pt'))
+
+        # Save Scaler
+        joblib.dump(scaler, os.path.join(ccf_processed_dir,'scaler.joblib'))
+
+        # Save Feature Names:
+        with open(os.path.join(ccf_processed_dir,'feature_names.txt'), 'w')as f:
+            for item in feature_names:
+                f.write('%s\n'% item)
+
+        print(f'Credit Card Fraud processed data and loaders save to: ')
+    except Exception as e:
+        print(f'Error processing Credit Card Fraud data: {e}')
+
+    print('\n --- Processing Synthetic Dataset ---')
+    try:
+        ccf_processed_dir = PROCESSED_DATA_PATH['synthetic_data_path']
+
+        X_train, y_train, X_val, y_val, X_test, y_test, scaler, feature_names = \
+            get_preprocessed_data(dataset_name="synthetic_data", target_column="isFraud")
+        
+        # Save Tensors 
+        torch.save(X_train, os.path.join(ccf_processed_dir, 'X_train.pt'))
+        torch.save(y_train, os.path.join(ccf_processed_dir, 'y_train.pt'))
+        torch.save(X_val, os.path.join(ccf_processed_dir, 'X_val.pt'))
+        torch.save(y_val, os.path.join(ccf_processed_dir, 'y_val.pt'))
+        torch.save(X_test, os.path.join(ccf_processed_dir, 'X_test.pt'))
+        torch.save(y_test, os.path.join(ccf_processed_dir, 'y_test.pt'))
+
+        # Save Scaler
+        joblib.dump(scaler, os.path.join(ccf_processed_dir,'scaler.joblib'))
+
+        # Save Feature Names:
+        with open(os.path.join(ccf_processed_dir,'feature_names.txt'), 'w')as f:
+            for item in feature_names:
+                f.write('%s\n'% item)
+
+        print(f'Synthetic Credit Card Fraud processed data and loaders save to: ')
+    except Exception as e:
+        print(f'Error processing synthetic Credit Card Fraud data: {e}')
